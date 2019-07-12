92c6f1fc8abd40c6a160149cf2391ac989912484
Mouha Diouf <mouhamadoudioufsn@gmail.com>
2019-07-12 12:28:25 +0000

    Addign the bubble_sort method

---
+++ methods.rb
@@ -0,0 +1,16 @@
+def bubble_sort(n)
+    return n if n.length <= 1
+    i = 0 
+    j = 0
+    while j < n.length-1
+    while i < n.length-1
+      if n[i] > n[i+1]
+      n[i], n[i+1] = n[i+1], n[i]
+      end 
+            i+= 1
+    end
+    j+= 1
+    i = 0
+    end
+    return n 
+  end
